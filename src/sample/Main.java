0cc04008fdc103f72dfb675a8e7804c504135cea
Tzanker <47015099+Tzanker@users.noreply.github.com>
2020-03-09 19:25:41 -0400

    Finally figured out how to add images as nodes and adding padding to Hboxes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,30 @@
 package sample;
-
+import javafx.scene.Group;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.ImageInput;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.transform.Translate;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.Canvas;
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        VBox root = FXMLLoader.load(getClass().getResource("background.fxml"));
         root.getStylesheets().add(getClass().getResource("./resources/Main.css").toExternalForm());
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root, 600, 512));
         primaryStage.show();
+
     }
 
 

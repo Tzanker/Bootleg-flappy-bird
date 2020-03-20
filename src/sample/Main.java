54bd8bf1e3b91e33c780669d27f57f5d64495b43
Tzanker <47015099+Tzanker@users.noreply.github.com>
2020-03-20 13:13:06 -0400

    Implemented sprites, very crude game is up and running

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,12 @@
 package sample;
 import javafx.animation.Animation;
 import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.effect.ImageInput;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.transform.Translate;
 import javafx.application.Application;
@@ -16,6 +18,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.canvas.Canvas;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
@@ -29,16 +32,48 @@ public class Main extends Application {
         root.getChildren().add(canvas);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         Image background = new Image(getClass().getResourceAsStream("resources/images/background.png"));
-        Image downpipe = new Image(getClass().getResourceAsStream("resources/images/down_pipe.png"));
-        Image uppipe = new Image(getClass().getResourceAsStream("resources/images/up_pipe.png"));
+        Sprite downpipe = new Sprite("resources/images/down_pipe.png");
+        Sprite uppipe = new Sprite("resources/images/up_pipe.png");
+        Sprite bird = new Sprite("resources/images/bird1.png");
+        bird.setPos(250, 256);
+        bird.setAcc(0,981);
+        ArrayList<Integer> input = new ArrayList<Integer>();
+        input.add(0);
+        scene.setOnKeyPressed(
+                new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent e) {
+                        String code = e.getCode().toString();
+                        if (code == "SPACE") {
+                            input.set(0,1);
+                        }
+                    }
+                }
+        );
+
         final long startNanoTime = System.nanoTime();
         new AnimationTimer(){
             public void handle(long currentNanoTime){
                 double t = (currentNanoTime - startNanoTime)/1000000000.0;
                 gc.drawImage(background, 0, 0);
                 gc.drawImage(background, 288, 0);
-                gc.drawImage(downpipe, 250 + 250*Math.sin(t), -600);
-                gc.drawImage(uppipe, 250 + 250*Math.sin(t), 400);
+                if(input.get(0)==1){
+                    input.set(0,0);
+                    bird.setVel(0,-400);
+                }
+                bird.update(t);
+                downpipe.setPos(250 + 250*Math.sin(t), -600);
+                uppipe.setPos(250 + 250*Math.sin(t), 400);
+                if(bird.intersects(uppipe) || bird.intersects(downpipe)){
+                    Image gameOver = new Image(getClass().getResourceAsStream("resources/images/game_over.png"));
+                    gc.drawImage(gameOver, 250, 256);
+                    bird.setVel(0,500);
+                }
+                uppipe.render(gc);
+                downpipe.render(gc);
+                bird.render(gc);
+
+                System.out.println(t);
             }
         }.start();
         primaryStage.show();
